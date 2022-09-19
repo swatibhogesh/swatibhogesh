d2bf8b39bd5b2a0c461d55e897242fccdcfc58e2
swatibhogesh <112920942+swatibhogesh@users.noreply.github.com>
2022-09-19 16:04:30 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,19 @@
+def palindrome(number):
+    print("original number", number)
+    original_num = number
+    
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number is a palindrome")
+    else:
+        print("Given number is not a palindrome")
+
+palindrome(121)
+palindrome(125)
