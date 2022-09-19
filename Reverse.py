9e7ca27b8ff8378421f767a536d68d925f7e5c8f
swatibhogesh <112920942+swatibhogesh@users.noreply.github.com>
2022-09-19 16:06:13 +0530

    Delete Reverse.py

--- Reverse.py
+++
@@ -1,8 +0,0 @@
-number = 7536
-print("Given number", number)
-while number > 0:
-    # get the last digit
-    digit = number % 10
-    # remove the last digit and repeat the loop
-    number = number // 10
-    print(digit, end=" ")
